be40c77078c5c0d6d2555098def2e1932b42ea26
Wilkenson <tcbwilkenson@gamilcom>
2023-11-01 07:12:21 -0400

    account bank setup

---
+++ sec2/Account.java
@@ -0,0 +1,15 @@
+class Account {
+    String owner;
+    double balance;
+    double minBalance; // minimum balance
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public double withdraw(double amount) {
+        double amt = 0; // amount to withdraw
+
+        return amt; // return the amount withdrawn
+    }
+}
---
+++ sec2/createClass.java
@@ -0,0 +1,7 @@
+package sec2;
+
+class Main {
+    public static void main(String[] argrs) {
+
+    }
+}

6b36f59ea37f15b3a33d390a69ad98d5e7430b67
Wilkenson <tcbwilkenson@gamilcom>
2024-01-12 13:25:13 -0500

    lambda and for each

--- functional/MethodRefExam.java
+++ functional/MethodRefExam.java
@@ -4,6 +4,7 @@ package functional;
 import java.io.FilterOutputStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -28,17 +29,48 @@ public class MethodRefExam {
         System.out.println(squareOddNum);
 
 //        count even numbers
-        System.out.println(TerminalOperation.count());
+        TerminalOperation.count();
+
+//         words fewer than 6 characters
+        IntermediateOperationsExample.listStr();
+
+//        lambda
+        LoopToForEachExample.loopToEach();
 
     }
 
     static class TerminalOperation {
-        public static long count() {
+        public static void count() {
             List<Integer> l = Arrays.asList(1, 2, 4, 5, 8, 9);
 
-            return l.stream()
+            long count = l.stream()
                     .filter(n -> n % 2 == 0)
                     .count();
+            System.out.println(count + " filter even and get count ");
+
+        }
+    }
+
+    static class IntermediateOperationsExample {
+        public static void listStr() {
+            List<String> str = Arrays.asList("Wilkenson", "Hilarion", "Lord", "wilk", "wi");
+            List<String> fewerThan6 = str.stream()
+                    .filter(n -> n.length() < 6)
+                    .map(String::toUpperCase)
+                    .collect(Collectors.toList());
+            System.out.println(fewerThan6 + " filter less than 6 ");
+
+        }
+    }
+
+    static class LoopToForEachExample {
+        public static void loopToEach(){
+            List<String> str = Arrays.asList("wilkenosn", "hilarion");
+            for (String s : str) {
+                System.out.println(s);
+            }
+            // use foreach and lambda exp
+            str.forEach(s -> System.out.println(s));
         }
     }
 }
